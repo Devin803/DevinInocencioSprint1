a9bec4a8da1847d585b35092facb9bbae9e2c334
devin <dinocencio@student.bridgew.edu>
2023-02-10 14:14:10 -0500

    Added a try/except to populate_db

--- database_data.py
+++ database_data.py
@@ -15,6 +15,7 @@ def close_db(connection: sqlite3.Connection):
     connection.close()
 
 
+# Creates a database for wufoo entry forms
 def setup_db(cursor: sqlite3.Cursor):
     cursor.execute('''CREATE TABLE IF NOT EXISTS wufoo_form
                       (EntryID INTEGER PRIMARY KEY,
@@ -33,22 +34,32 @@ def setup_db(cursor: sqlite3.Cursor):
                       Updated_By TEXT);''')
 
 
+# Populates the database with wufoo form data
 def populate_db(cursor: sqlite3.Cursor):
     form_data = api_data.get_wufoo_data()
-    for item in form_data['Entries']:
-        cursor.execute(f'''INSERT INTO wufoo_form VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)''',
-                       (item['EntryId'],
-                        item.get('Field17', ''),
-                        item.get('Field18', ''),
-                        item.get('Field4', ''),
-                        item.get('Field7', ''),
-                        item.get('Field20', ''),
-                        item.get('Field8', ''),
-                        item.get('Field9', ''),
-                        item.get('Field23', ''),
-                        item.get('Field121', ''),
-                        item.get('Field224', ''),
-                        item.get('DateCreated', ''),
-                        item.get('CreatedBy', ''),
-                        item.get('UpdatedBy', '')))
-    cursor.connection.commit()
+    try:
+        for item in form_data['Entries']:
+            cursor.execute(f'''INSERT INTO wufoo_form VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)''',
+                           (item['EntryId'],
+                            item.get('Field17', ''),
+                            item.get('Field18', ''),
+                            item.get('Field4', ''),
+                            item.get('Field7', ''),
+                            item.get('Field20', ''),
+                            item.get('Field8', ''),
+                            item.get('Field9', ''),
+                            item.get('Field23', ''),
+                            item.get('Field121', ''),
+                            item.get('Field224', ''),
+                            item.get('DateCreated', ''),
+                            item.get('CreatedBy', ''),
+                            item.get('UpdatedBy', '')))
+        cursor.connection.commit()
+        cursor.connection.close()
+    except sqlite3.Error as error:
+        print(f'Database error occurred {error}')  # Currently gives me an error, but still runs the code correctly
+    finally:
+        cursor.connection.commit()
+        if cursor.connection:
+            cursor.connection.close()
+            print('Database connection closed.')
